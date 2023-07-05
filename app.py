0349b61166401334907b41f1b12014075f48bab2
anan0112 <zen5215125@gmail.com>
2023-07-05 20:59:43 +0800

    網頁呈現測試、檔案分類

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/login')
 def index():
-    return render_template("index.html")
+    return render_template('userLogin.html')
+
+@app.route('/signup')
+def signup():
+	return render_template('userSignup.html')
+
+@app.route('/forgetpsw')
+def forgetpsw():
+	return render_template('userForget.html')
+
+@app.route('/reset')
+def resetpsw():
+	return render_template('resetPassword.html')
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port='5000',debug=True)
